0da6ea108f489df0c2ccdb69b0c05c209dbedaa5
Omer Yanai <omery33111@gmail.com>
2023-09-03 23:45:08 +0300

    blog done

--- front-end/src/features/blog/Blog.tsx
+++ front-end/src/features/blog/Blog.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from 'react';
-import { getBlogsAsync, selectBlogs, selectLikes, toggleLike } from './blogSlice';
+import {
+  getBlogsAsync,
+  selectBlogs,
+  selectLikes,
+  toggleLike,
+} from './blogSlice';
 import { myServer } from '../../endpoints/endpoints';
 import { useAppDispatch, useAppSelector } from '../../app/hooks';
 import { FaHeart } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
-
 
 const Blog = () => {
   const dispatch = useAppDispatch();
@@ -16,8 +20,6 @@ const Blog = () => {
 
   const likesRecord = likes as Record<string, number>; // Assert the type
 
-
-
   useEffect(() => {
     dispatch(getBlogsAsync());
 
@@ -28,14 +30,12 @@ const Blog = () => {
     }
   }, [dispatch]);
 
-
   const [containerHeight, setContainerHeight] = useState(10);
 
   useEffect(() => {
     const newContainerHeight = 10 + blogs.length * 160;
     setContainerHeight(newContainerHeight);
   }, [blogs]);
-
 
   function formatDate(dateString: any) {
     const date = new Date(dateString);
@@ -46,7 +46,6 @@ const Blog = () => {
     const minutes = date.getMinutes().toString().padStart(2, '0');
     return `${year}-${month}-${day}, ${hours}:${minutes}`;
   }
-
 
   return (
     <div>
@@ -59,64 +58,63 @@ const Blog = () => {
           backgroundRepeat: 'repeat-y',
           height: `${containerHeight}vh`,
           width: '100%',
-        }}>
+        }}
+      >
         <div className="d-flex justify-content-center">
           <div className="blog-videos">
-            
             <div style={{ height: 300 }} />
             {blogs.slice().reverse().map((blog, index) => {
               const topPosition = 100 * index;
+              const blogClassName = index > 0 ? 'blog-item' : ''; // Set classname for the second blog and onwards
+              const blogStyle = index > 0 ? { marginTop: '60%' } : { marginBottom: '60%' }; // Set style for the second blog and onwards
               return (
                 <div
                   key={index}
+                  className={blogClassName}
                   style={{
-                    marginBottom: '60%',
-                    marginTop: 0,
+                    ...blogStyle,
                     position: 'relative',
                     top: `${topPosition}px`,
-                  }}>
-
-
-                <div className='blog-time-line'>
-                  <img
-                    src={`${require('../../images/blogtimeline.png')}`}
-                    width="100%"
-                    height="100%"
-                  />
-                  <h6 className = "blog-date">{formatDate(blog.date)}</h6>
+                  }}
+                >
+                  <div className="blog-time-line">
+                    <img
+                      src={`${require('../../images/blogtimeline.png')}`}
+                      width="100%"
+                      height="100%"
+                    />
+                    <h6 className="blog-date">{formatDate(blog.date)}</h6>
                   </div>
-
-                <div>
-                  <img
-                    src={myServer + blog.picture}
-                    width="100%"
-                    height="100%"
-                  />
+                  <div>
+                    <img
+                      src={myServer + blog.picture}
+                      width="100%"
+                      height="100%"
+                    />
                   </div>
-
-                    <div>
-
-                <img 
-                onClick = {() => navigate(`/blog/blog_page/${blog.id}`)}
-                src={require('../../images/forthumbnail.png')} width="100%"
-                    alt="profile-logo" 
-                    style = {{position: "absolute", top: 0, right: 0}}/>
-
+                  <div>
+                    <img
+                      onClick={() => navigate(`/blog/blog_page/${blog.id}`)}
+                      src={require('../../images/forthumbnail.png')}
+                      width="100%"
+                      alt="profile-logo"
+                      style={{ position: 'absolute', top: 0, right: 0 }}
+                    />
                   </div>
-
-
-                  <h2 style = {{padding: "10px"}}>{blog.title}</h2>
-                  <p style = {{padding: "7px"}}>{blog.description}</p>
-                            <h3>
-                <FaHeart
-                  className={`like-button ${likesRecord[blog.id] ? 'liked' : ''}`}
-                  onClick={() => dispatch(toggleLike({ blogId: blog.id }))}
-                />
-                </h3>
-                <div className='counter-like' style = {{color: "grey"}}>
-                {likesRecord[blog.id] || 0}
-                </div>
-                    {/* <BlogComments /> */}
+                  <h2 style={{ padding: '10px' }}>{blog.title}</h2>
+                  <p style={{ padding: '7px' }}>{blog.description}</p>
+                  <h3>
+                    <FaHeart
+                      className={`like-button ${
+                        likesRecord[blog.id] ? 'liked' : ''
+                      }`}
+                      onClick={() => dispatch(toggleLike({ blogId: blog.id }))}
+                    />
+                  </h3>
+                  <div className="counter-like" style={{ color: 'grey' }}>
+                    {likesRecord[blog.id] || 0}
+                  </div>
+                  {/* <BlogComments /> */}
                 </div>
               );
             })}
