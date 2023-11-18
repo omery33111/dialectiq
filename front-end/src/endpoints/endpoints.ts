c917edc6b2a4e8b82632c9c0463ebda52a664343
Omer Yanai <omery33111@gmail.com>
2023-11-18 19:48:24 +0200

    configuration

--- front-end/src/endpoints/endpoints.ts
+++ front-end/src/endpoints/endpoints.ts
@@ -2,91 +2,91 @@
 
 
 // ------------------------- SERVER ENDPOINT:
-export const myServer = "http://127.0.0.1:8000";
+// export const myServer = "http://127.0.0.1:8000";
 
-// export const myServer = "https://dialectiq-back-end.onrender.com";
+export const myServer = "https://dialectiq-back-end.onrender.com";
 
 
 
 
 
 // ------------------------- AUTHENTICATION ENDPOINTS:
-export const loginURL = "http://127.0.0.1:8000/authentication/login/";
+// export const loginURL = "http://127.0.0.1:8000/authentication/login/";
 
-export const registerURL = "http://127.0.0.1:8000/authentication/register/";
+// export const registerURL = "http://127.0.0.1:8000/authentication/register/";
 
-// export const loginURL = "https://dialectiq-back-end.onrender.com/authentication/login/";
+export const loginURL = "https://dialectiq-back-end.onrender.com/authentication/login/";
 
-// export const registerURL = "https://dialectiq-back-end.onrender.com/authentication/register/";
+export const registerURL = "https://dialectiq-back-end.onrender.com/authentication/register/";
 
 
 
 
 
 // ------------------------- PROFILE ENDPOINTS:
-export const profileURL = "http://127.0.0.1:8000/profile_user";
+// export const profileURL = "http://127.0.0.1:8000/profile_user";
 
-// export const profileURL = "https://dialectiq-back-end.onrender.com/profile_user";
-
-
-
-
-
-// ------------------------- CALLBACK ENDPOINTS:
-export const callbackURL = "http://127.0.0.1:8000/callback";
-
-// export const callbackURL = "https://dialectiq-back-end.onrender.com/callback";
+export const profileURL = "https://dialectiq-back-end.onrender.com/profile_user";
 
 
 
 
 
 // ------------------------- CALLBACK ENDPOINTS:
-export const administratorURL = "http://127.0.0.1:8000/administrator";
+// export const callbackURL = "http://127.0.0.1:8000/callback";
 
-// export const administratorURL = "https://dialectiq-back-end.onrender.com/administrator";
+export const callbackURL = "https://dialectiq-back-end.onrender.com/callback";
+
+
+
+
+
+// ------------------------- CALLBACK ENDPOINTS:
+// export const administratorURL = "http://127.0.0.1:8000/administrator";
+
+export const administratorURL = "https://dialectiq-back-end.onrender.com/administrator";
 
 
 
 
 
 // ------------------------- BLOG ENDPOINTS:
-export const blogURL = "http://127.0.0.1:8000/blog";
+// export const blogURL = "http://127.0.0.1:8000/blog";
 
-// export const blogURL = "https://dialectiq-back-end.onrender.com/blog";
+export const blogURL = "https://dialectiq-back-end.onrender.com/blog";
 
 
 
 
 
 // ------------------------- COMMENT ENDPOINTS:
-export const commentURL = "http://127.0.0.1:8000/comment";
+// export const commentURL = "http://127.0.0.1:8000/comment";
 
-// export const commentURL = "https://dialectiq-back-end.onrender.com/comment";
+export const commentURL = "https://dialectiq-back-end.onrender.com/comment";
 
 
 
 
 
 // ------------------------- AMERICAN ENDPOINTS:
-export const americanURL = "http://127.0.0.1:8000/quiz_american";
+// export const americanURL = "http://127.0.0.1:8000/quiz_american";
 
-// export const americanURL = "https://dialectiq-back-end.onrender.com/quiz_american";
+export const americanURL = "https://dialectiq-back-end.onrender.com/quiz_american";
 
 
 
 
 
 // ------------------------- COMPLETE THE SENTENCE ENDPOINTS:
-export const sentenceURL = "http://127.0.0.1:8000/quiz_sentence";
+// export const sentenceURL = "http://127.0.0.1:8000/quiz_sentence";
 
-// export const sentenceURL = "https://dialectiq-back-end.onrender.com/quiz_sentence";
+export const sentenceURL = "https://dialectiq-back-end.onrender.com/quiz_sentence";
 
 
 
 
 
 // ------------------------- VOICE QUIZ ENDPOINTS:
-export const voiceURL = "http://127.0.0.1:8000/quiz_voice";
+// export const voiceURL = "http://127.0.0.1:8000/quiz_voice";
 
-// export const voiceURL = "https://dialectiq-back-end.onrender.com/quiz_voice";
+export const voiceURL = "https://dialectiq-back-end.onrender.com/quiz_voice";
