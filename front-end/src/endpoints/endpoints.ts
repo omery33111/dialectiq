9b34d6a37e02857dff2f5ec4bccd3cd91419a5e1
Omer Yanai <omery33111@gmail.com>
2023-11-18 19:45:48 +0200

    configuration

--- front-end/src/endpoints/endpoints.ts
+++ front-end/src/endpoints/endpoints.ts
@@ -4,7 +4,7 @@
 // ------------------------- SERVER ENDPOINT:
 export const myServer = "http://127.0.0.1:8000";
 
-// export const myServer = "";
+// export const myServer = "https://dialectiq-back-end.onrender.com";
 
 
 
@@ -15,9 +15,9 @@ export const loginURL = "http://127.0.0.1:8000/authentication/login/";
 
 export const registerURL = "http://127.0.0.1:8000/authentication/register/";
 
-// export const loginURL = "/authentication/login/";
+// export const loginURL = "https://dialectiq-back-end.onrender.com/authentication/login/";
 
-// export const registerURL = "/authentication/register/";
+// export const registerURL = "https://dialectiq-back-end.onrender.com/authentication/register/";
 
 
 
@@ -26,7 +26,7 @@ export const registerURL = "http://127.0.0.1:8000/authentication/register/";
 // ------------------------- PROFILE ENDPOINTS:
 export const profileURL = "http://127.0.0.1:8000/profile_user";
 
-// export const profileURL = "/profile_user";
+// export const profileURL = "https://dialectiq-back-end.onrender.com/profile_user";
 
 
 
@@ -35,7 +35,7 @@ export const profileURL = "http://127.0.0.1:8000/profile_user";
 // ------------------------- CALLBACK ENDPOINTS:
 export const callbackURL = "http://127.0.0.1:8000/callback";
 
-// export const callbackURL = "/callback";
+// export const callbackURL = "https://dialectiq-back-end.onrender.com/callback";
 
 
 
@@ -44,7 +44,7 @@ export const callbackURL = "http://127.0.0.1:8000/callback";
 // ------------------------- CALLBACK ENDPOINTS:
 export const administratorURL = "http://127.0.0.1:8000/administrator";
 
-// export const administratorURL = "/administrator";
+// export const administratorURL = "https://dialectiq-back-end.onrender.com/administrator";
 
 
 
@@ -53,7 +53,7 @@ export const administratorURL = "http://127.0.0.1:8000/administrator";
 // ------------------------- BLOG ENDPOINTS:
 export const blogURL = "http://127.0.0.1:8000/blog";
 
-// export const blogURL = "/blog";
+// export const blogURL = "https://dialectiq-back-end.onrender.com/blog";
 
 
 
@@ -62,7 +62,7 @@ export const blogURL = "http://127.0.0.1:8000/blog";
 // ------------------------- COMMENT ENDPOINTS:
 export const commentURL = "http://127.0.0.1:8000/comment";
 
-// export const commentURL = "/comment";
+// export const commentURL = "https://dialectiq-back-end.onrender.com/comment";
 
 
 
@@ -71,7 +71,7 @@ export const commentURL = "http://127.0.0.1:8000/comment";
 // ------------------------- AMERICAN ENDPOINTS:
 export const americanURL = "http://127.0.0.1:8000/quiz_american";
 
-// export const americanURL = "/quiz_american";
+// export const americanURL = "https://dialectiq-back-end.onrender.com/quiz_american";
 
 
 
@@ -80,7 +80,7 @@ export const americanURL = "http://127.0.0.1:8000/quiz_american";
 // ------------------------- COMPLETE THE SENTENCE ENDPOINTS:
 export const sentenceURL = "http://127.0.0.1:8000/quiz_sentence";
 
-// export const sentenceURL = "/quiz_sentence";
+// export const sentenceURL = "https://dialectiq-back-end.onrender.com/quiz_sentence";
 
 
 
@@ -89,4 +89,4 @@ export const sentenceURL = "http://127.0.0.1:8000/quiz_sentence";
 // ------------------------- VOICE QUIZ ENDPOINTS:
 export const voiceURL = "http://127.0.0.1:8000/quiz_voice";
 
-// export const voiceURL = "/quiz_voice";
+// export const voiceURL = "https://dialectiq-back-end.onrender.com/quiz_voice";
