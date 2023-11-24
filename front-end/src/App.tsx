edb1647abc23ee7100e6638786a1d1ffa69458ca
Omer Yanai <omery33111@gmail.com>
2023-11-24 15:07:55 +0200

    finishes

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -11,9 +11,9 @@ import { getMyIDAsync, selectUserID, setMyID } from './features/profile/profileS
 function App() {
   const dispatch = useAppDispatch();
   const [initialFetchDone, setInitialFetchDone] = useState(false);
+  const [lastClickTimes, setLastClickTimes] = useState<number[]>([]);
+  const [logoutTimer, setLogoutTimer] = useState<NodeJS.Timeout | null>(null);
 
-  const storedmyID = JSON.parse(localStorage.getItem('myID') as string);
-  const storedIsLogged = JSON.parse(localStorage.getItem('token') as string);
   const userID = useAppSelector(selectUserID);
   const isLogged = useAppSelector(selectIsLogged);
 
@@ -24,8 +24,6 @@ function App() {
           setInitialFetchDone(true);
         });
     }
-
-  
   }, [dispatch, userID, initialFetchDone]);
 
   useEffect(() => {
@@ -33,6 +31,40 @@ function App() {
       dispatch(setMyID(userID));
     }
   }, [dispatch, userID, initialFetchDone]);
+
+  useEffect(() => {
+    // Retrieve lastClickTimes from local storage if available
+    const storedClickTimes = JSON.parse(localStorage.getItem('lastClickTimes') || '[]');
+    setLastClickTimes(storedClickTimes);
+
+    const handleMouseClick = () => {
+      const currentTime = Date.now();
+      setLastClickTimes(prevTimes => {
+        const updatedTimes = [currentTime, ...(prevTimes.length >= 2 ? prevTimes.slice(0, 1) : prevTimes)];
+        localStorage.setItem('lastClickTimes', JSON.stringify(updatedTimes));
+        return updatedTimes;
+      });
+
+      if (lastClickTimes.length === 2) {
+        const timeGap = currentTime - lastClickTimes[1];
+        if (timeGap > 120 * 60 * 1000) { // 120 minutes in milliseconds
+          onLogout();
+        }
+      }
+    };
+
+    document.addEventListener('click', handleMouseClick);
+
+    return () => {
+      document.removeEventListener('click', handleMouseClick);
+    };
+  }, [lastClickTimes]);
+
+  const onLogout = () => {
+    dispatch(logoutAsync());
+    localStorage.clear(); // Clear all local storage data upon logout
+    window.location.href = "/";
+  };
 
   return (
     <div className="App">
@@ -49,9 +81,7 @@ function App() {
       />
 
       <MyNavbar />
-
       <Outlet />
-
       <MyFooter />
     </div>
   );
