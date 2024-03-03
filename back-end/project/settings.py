c090f1707300660145ba8d325e3a4f83741e3397
Omer Yanai <omery33111@gmail.com>
2024-03-03 16:59:56 +0200

    finals

--- back-end/project/settings.py
+++ back-end/project/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -193,11 +193,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",  # Add the origins that are allowed
-    # Add more origins as needed
-]
-
 
 CORS_ALLOW_ALL_ORIGINS = True
