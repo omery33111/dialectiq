555c4ff8d0a1d42faedfd743f312133332e2b24c
Omer Yanai <omery33111@gmail.com>
2024-03-03 17:04:12 +0200

    finals

--- back-end/project/settings.py
+++ back-end/project/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    # 'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
@@ -193,5 +193,13 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOWED_ORIGINS = [
+    "https://the-hebrew-adventure.netlify.app"
+]
+
+CORS_ALLOWED_ORIGIN_REGEXES = [
+    r"^https://the-hebrew-adventure\.netlify\.app/$",
+]
 
 CORS_ALLOW_ALL_ORIGINS = True
