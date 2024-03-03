a47ed99b5662c910a55cba90dba984eec4dff3a5
Omer Yanai <omery33111@gmail.com>
2024-03-03 18:08:25 +0200

    finals

--- back-end/project/settings.py
+++ back-end/project/settings.py
@@ -89,7 +89,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    # 'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
